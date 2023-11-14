525def04c40d105b892280d9d50636b96fb1e347
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-14 21:09:51 +0900

    feat: enum 클래스 수정

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -1,13 +1,13 @@
 package christmas.domain;
 
 public enum Menu {
-	MUSHROOMSOUP("애피타이저","양송이스프",6_000),
+	MUSHROOMSOUP("애피타이저", "양송이스프", 6_000),
 	TAPAS("애피타이저", "타파스", 5_500),
 	CAESARSALAD("애피타이저", "시저샐러드", 8_000),
-	TBONESTEAK("메인","티본스테이크",55_000),
-	BBQRIBS("메인","바비큐립", 54_000),
+	TBONESTEAK("메인", "티본스테이크", 55_000),
+	BBQRIBS("메인", "바비큐립", 54_000),
 	SEAFOODPASTA("메인", "해산물파스타", 35_000),
-	CHRISTMASPASTA("메인","크리스마스파스타", 25_000),
+	CHRISTMASPASTA("메인", "크리스마스파스타", 25_000),
 	CHOCOCAKE("디저트", "초코케이크", 15_000),
 	ICECREAM("디저트", "아이스크림", 5_000),
 	ZEROCOLA("음료", "제로콜라", 3_000),
@@ -18,21 +18,21 @@ public enum Menu {
 	private final String name;
 	private final int price;
 
-	Menu(String type, String name, int price){
+	Menu(String type, String name, int price) {
 		this.type = type;
 		this.name = name;
 		this.price = price;
 	}
 
-	public String getType(){
-		return this.type;
+	public String getType() {
+		return type;
 	}
 
-	public String getName(){
-		return this.name;
+	public String getName() {
+		return name;
 	}
 
-	public int getPrice(){
-		return this.price;
+	public int getPrice() {
+		return price;
 	}
 }
