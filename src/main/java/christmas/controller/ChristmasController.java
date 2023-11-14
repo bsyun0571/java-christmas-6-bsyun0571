837eaeb9519ef44bc1520ad4709bec352898e8a9
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-14 22:48:36 +0900

    refactor: 올바른 순서로 출력하도록 수정 및 필요없는 메소드 제거

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -12,37 +12,33 @@ public class ChristmasController {
 	public void startEvent() {
 		OutputView.printGreet();
 		int date = EventDate.inputDate();
-		OutputView.printStart(date);
 		Map<String, Integer> menu = EventMenu.setMenuList();
+		OutputView.printStart(date);
+		OutputView.printMenu(menu);
 		int amount = getAmount(menu);
-
-		printEventPlanner(menu, amount);
+		OutputView.printAmountBeforeDiscount(amount);
 		applyEvent(date, menu, amount);
-
 	}
 
 	private void applyEvent(int date, Map<String, Integer> menu, int amount) {
-		int christmasSaleMoney = Event.christmasDDaySale(date,amount);
+		int christmasSaleMoney = Event.christmasDDaySale(date, amount);
 		int daySaleMoney = Event.daySale(date, menu, amount);
 		int specialSaleMoney = Event.specialSale(date, amount);
 		int giftMoney = Event.giveGift(amount);
 		OutputView.printGiftMenu(giftMoney);
 		OutputView.printBenefitsDetails(christmasSaleMoney, daySaleMoney, specialSaleMoney, giftMoney, date);
+		OutputView.printTotalBenefitsAmount(christmasSaleMoney, daySaleMoney, specialSaleMoney, giftMoney);
+		OutputView.printAmountAfterDiscount(christmasSaleMoney, daySaleMoney, specialSaleMoney, amount);
+		OutputView.printEventBadge(christmasSaleMoney,daySaleMoney,specialSaleMoney,giftMoney);
 	}
 
 
 	private int getAmount(Map<String, Integer> menu) {
 		int amount = 0;
-		for(String key : menu.keySet()){
+		for (String key : menu.keySet()) {
 			amount += Menu.getMenuInfo(key)
 					.getPrice() * menu.get(key);
 		}
 		return amount;
-	}
-
-
-	private void printEventPlanner(Map<String, Integer> menu, int amount) {
-		OutputView.printAmountBeforeDiscount(amount);
-
 	}
 }
