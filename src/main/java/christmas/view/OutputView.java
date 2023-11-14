5d16a348fe3c8c6f8fa9462ce82393bc29f9993f
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-14 21:40:34 +0900

    feat: 할인 전 총주문 금액 출력 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,22 +1,35 @@
 package christmas.view;
 
+import christmas.domain.Menu;
+import java.util.Map;
+
 public class OutputView {
 
 	public static void printGreet() {
 		System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
-		System.out.println();
 	}
 
 	public static void printStart(int date) {
 		System.out.println("12월 " + date + "일에 우테코 식당에서 받을 이벤트 혜택 미리보기!");
 
 	}
-	public static void printMenu() {
+	public static void printMenu(Map<String, Integer> menu) {
 		System.out.println("<주문 메뉴>");
-
+		for (String key : menu.keySet()){
+			System.out.println(key + " " + menu.get(key) + "개");
+		}
+		System.out.println();
 	}
-	public static void printAmountBeforeDiscount() {
+	public static void printAmountBeforeDiscount(Map<String, Integer> menu) {
 		System.out.println("<할인 전 총주문 금액>");
+		int amount = 0;
+
+		for(String key : menu.keySet()){
+			amount += Menu.getMenuInfo(key)
+					.getPrice();
+		}
+		System.out.printf("%,d원\n", amount);
+		System.out.println();
 
 	}
 	public static void printGiftMenu() {
