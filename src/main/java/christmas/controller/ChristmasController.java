6e307702f5c07b0a8dce51305080570e8a4b0b21
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-14 22:32:06 +0900

    feat: 이벤트 적용 구현

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -4,6 +4,7 @@ package christmas.controller;
 import christmas.domain.Event;
 import christmas.domain.EventDate;
 import christmas.domain.EventMenu;
+import christmas.domain.Menu;
 import christmas.view.OutputView;
 import java.util.Map;
 
@@ -13,19 +14,35 @@ public class ChristmasController {
 		int date = EventDate.inputDate();
 		OutputView.printStart(date);
 		Map<String, Integer> menu = EventMenu.setMenuList();
+		int amount = getAmount(menu);
 
+		printEventPlanner(menu, amount);
+		applyEvent(date, menu, amount);
 
-		Event.applyEvent(date, menu);
-		printEventPlanner(menu);
+	}
+
+	private void applyEvent(int date, Map<String, Integer> menu, int amount) {
+		int christmasSaleMoney = Event.christmasDDaySale(date,amount);
+		int daySaleMoney = Event.daySale(date, menu, amount);
+		int specialSaleMoney = Event.specialSale(date, amount);
+		int giftMoney = Event.giveGift(amount);
+		OutputView.printGiftMenu(giftMoney);
+		OutputView.printBenefitsDetails(christmasSaleMoney, daySaleMoney, specialSaleMoney, giftMoney, date);
 	}
 
 
-	private void printEventPlanner(Map<String, Integer> menu) {
-		OutputView.printAmountBeforeDiscount(menu);
-		OutputView.printGiftMenu();
-		OutputView.printBenefitsDetails();
-		OutputView.printTotalBenefitsAmount();
-		OutputView.printAmountAfterDiscount();
-		OutputView.printEventBadge();
+	private int getAmount(Map<String, Integer> menu) {
+		int amount = 0;
+		for(String key : menu.keySet()){
+			amount += Menu.getMenuInfo(key)
+					.getPrice() * menu.get(key);
+		}
+		return amount;
+	}
+
+
+	private void printEventPlanner(Map<String, Integer> menu, int amount) {
+		OutputView.printAmountBeforeDiscount(amount);
+
 	}
 }
--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -1,9 +1,69 @@
 package christmas.domain;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class Event {
-	public static void applyEvent(int date, Map<String, Integer> menu) {
 
+	public static int giveGift(int amount) {
+		if (amount > 120000) {
+			return 25000;
+		}
+		return 0;
+	}
+
+	public static int specialSale(int date, int amount) {
+		if (amount < 10000) {
+			return 0;
+		}
+		if (date % 7 == 3 || date == 25) {
+			return 1000;
+		}
+		return 0;
+	}
+
+	public static int daySale(int date, Map<String, Integer> menu, int amount) {
+		if (amount < 10000) {
+			return 0;
+		}
+		if (date % 7 == 1 || date % 7 == 2) {
+			return weekendSale(date, menu);
+		}
+		if (date % 7 != 1 && date % 7 != 2) {
+			return weekdaySale(date, menu);
+		}
+		return 0;
+	}
+
+	public static int weekdaySale(int date, Map<String, Integer> menu) {
+		int saleMoney = 0;
+		for (String key : menu.keySet()) {
+			if (Objects.equals(Menu.getMenuInfo(key)
+					.getType(), "디저트")) {
+				saleMoney += 2023 * menu.get(key);
+			}
+		}
+		return saleMoney;
+	}
+
+	public static int weekendSale(int date, Map<String, Integer> menu) {
+		int saleMoney = 0;
+		for (String key : menu.keySet()) {
+			if (Objects.equals(Menu.getMenuInfo(key)
+					.getType(), "메인")) {
+				saleMoney += 2023;
+			}
+		}
+		return saleMoney;
+	}
+
+	public static int christmasDDaySale(int date, int amount) {
+		if (amount < 10000) {
+			return 0;
+		}
+		if (date < 26) {
+			return 1000 + (date - 1) * 100;
+		}
+		return 0;
 	}
 }
