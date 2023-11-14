cc91a11d9dfaa17ed62910ba56a32266571285a4
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-14 17:00:52 +0900

    feat: InputView, OutputView 호출을 통한 출력 및 입력 메소드 생성 및 구현

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,6 +1,26 @@
 package christmas.controller;
 
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class ChristmasController {
 	public void startEvent() {
+		OutputView.printGreet();
+		int date = InputView.readDate();
+		String menu = InputView.readMenu();
+		OutputView.printStart(date);
+		printEventPlanner();
+
+
+	}
+
+	private void printEventPlanner() {
+		OutputView.printMenu();
+		OutputView.printAmountBeforeDiscount();
+		OutputView.printGiftMenu();
+		OutputView.printBenefitsDetails();
+		OutputView.printTotalBenefitsAmount();
+		OutputView.printAmountAfterDiscount();
+		OutputView.printEventBadge();
 	}
 }
