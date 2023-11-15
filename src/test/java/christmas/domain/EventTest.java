6d9823aa4f543479c358a8d60ff12904914795bc
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-15 15:15:35 +0900

    feat: 할인 적용 테스트 작성 및 테스트 확인

--- src/test/java/christmas/domain/EventTest.java
+++ src/test/java/christmas/domain/EventTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class EventTest {
-	@DisplayName("수가 아닌 입력에 대한 예외 처리")
+	@DisplayName("증정 메뉴 - 수가 아닌 입력에 대한 예외 처리")
 	@ValueSource(strings = {"a", "--", "1000a"})
 	@ParameterizedTest
 	void isNumberInputGiveGift(String input){
@@ -19,21 +19,53 @@ public class EventTest {
 				.isInstanceOf(IllegalArgumentException.class);
 	}
 
-	@DisplayName("수가 아닌 입력에 대한 예외 처리")
+	@DisplayName("증정 메뉴 - 12만원 이하의 금액에 대한 예외 처리")
+	@Test
+	void underInputGiveGift(){
+		int amount = Event.giveGift(88500);
+		assertThat(amount).isEqualTo(0);
+	}
+
+	@DisplayName("특별 할인 - 수가 아닌 입력에 대한 예외 처리")
 	@Test
 	void isNumberInputSpecialSale(){
 		assertThatThrownBy(() -> Event.specialSale(3, Integer.parseInt("1000a")))
 				.isInstanceOf(IllegalArgumentException.class);
 	}
 
-	@DisplayName("수가 아닌 입력에 대한 예외 처리")
+	@DisplayName("특별 할인 - 수가 아닌 입력에 대한 예외 처리2")
 	@Test
 	void isNumberInputSpecialSale2(){
 		assertThatThrownBy(() -> Event.specialSale(Integer.parseInt("a"), Integer.parseInt("1000")))
 				.isInstanceOf(IllegalArgumentException.class);
 	}
 
-	@DisplayName("수가 아닌 입력에 대한 예외 처리")
+	@DisplayName("특별 할인 - 만원 이하의 금액에 대한 예외 처리")
+	@Test
+	void underInputSpecialSale(){
+		int amount = Event.specialSale(3,8500);
+		assertThat(amount).isEqualTo(0);
+	}
+
+	@DisplayName("특별 할인 - 정상 작동 테스트")
+	@ValueSource(strings = {"3", "10", "17", "24", "25"})
+	@ParameterizedTest
+	void executeAllSpecialSale(String input){
+		int amount = Event.specialSale(Integer.parseInt(input), 15000);
+		assertThat(amount).isEqualTo(1000);
+	}
+
+	@DisplayName("특별 할인 - 정상 작동 테스트")
+	@ValueSource(strings = {"26", "1", "4", "13"})
+	@ParameterizedTest
+	void executeNotSpecialSale(String input){
+		int amount = Event.specialSale(Integer.parseInt(input), 15000);
+		assertThat(amount).isEqualTo(0);
+	}
+
+
+
+	@DisplayName("평일/주말 할인 - 수가 아닌 입력에 대한 예외 처리")
 	@Test
 	void isNumberInputDaySale(){
 		Map<String, Integer> menu = new HashMap<>();
@@ -42,15 +74,54 @@ public class EventTest {
 				.isInstanceOf(IllegalArgumentException.class);
 	}
 
-	@DisplayName("메인 메뉴가 여러개일때 되는지 테스트")
+	@DisplayName("평일/주말 할인 - 만원 이하의 금액에 대한 예외 처리")
 	@Test
-	void isExistInputDaySale(){
+	void underInputDaySale(){
 		Map<String, Integer> menu = new HashMap<>();
-		menu.put("티본스테이크", 3);
-		int amount = Event.daySale(1, menu, 13000);
-		assertThat(amount == 2023 * 3);
+		menu.put("아이스크림", 1);
+		int amount = Event.daySale(3,menu,5000);
+		assertThat(amount).isEqualTo(0);
 	}
 
+	@DisplayName("평일/주말 할인 - 주말 + 메인 메뉴가 여러개일때 할인이 제대로 적용되는지 테스트")
+	@Test
+	void multiInputWeekendSale(){
+		Map<String, Integer> menu = new HashMap<>();
+		menu.put("티본스테이크", 3);
+		int amount = Event.daySale(1, menu, 165000);
+		assertThat(amount).isEqualTo(2023 * 3);
+	}
+
+	@DisplayName("평일/주말 할인 - 평일 + 디저트 메뉴가 여러개일때 할인이 제대로 적용되는지 테스트")
+	@Test
+	void multiInputWeekdaySale(){
+		Map<String, Integer> menu = new HashMap<>();
+		menu.put("초코케이크", 5);
+		int amount = Event.daySale(4, menu, 75000);
+		assertThat(amount).isEqualTo(2023 * 5);
+	}
+
+	@DisplayName("크리스마스 디데이 할인 - 수가 아닌 입력에 대한 예외 처리")
+	@Test
+	void isNumberInputChristmasDDaySale(){
+		assertThatThrownBy(() -> Event.christmasDDaySale(3, Integer.parseInt("1000a")))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("만원 이하의 금액에 대한 예외 처리")
+	@Test
+	void underInputChristmasDDaySale(){
+		int amount = Event.christmasDDaySale(3,8500);
+		assertThat(amount).isEqualTo(0);
+	}
+
+	@DisplayName("크리스마스 할인 - 정상 작동 테스트")
+	@ValueSource(strings = {"26", "27", "28", "29", "30", "31"})
+	@ParameterizedTest
+	void executeNotChristmasDDaySale(String input){
+		int amount = Event.christmasDDaySale(Integer.parseInt(input), 15000);
+		assertThat(amount).isEqualTo(0);
+	}
 
 
 }
